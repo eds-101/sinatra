53c49ce4bfef78562fbcfe011acd65153146effd
Eds <steadyede@live.co.uk>
2021-02-19 10:26:51 +0000

    pic added

--- hello.rb
+++ hello.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
 
 get '/' do
-  'hello!'
+  "<div>
+  <img src='https://talksport.com/wp-content/uploads/sites/5/2020/09/NINTCHDBPICT000604896695-e1599051628702.jpg?strip=all&w=960&quality=100'>
+ </div>"
+end
+
+get '/secret' do
+  'This is a secret page'
 end
